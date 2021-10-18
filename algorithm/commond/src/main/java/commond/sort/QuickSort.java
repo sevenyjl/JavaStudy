44e1dc787c0ff8eb3da960e768c900ec942b9f63
sevenyjl <50271500+sevenyjl@users.noreply.github.com>
2021-10-18 15:07:52 +0800

    Update QuickSort.java

--- algorithm/commond/src/main/java/commond/sort/QuickSort.java
+++ algorithm/commond/src/main/java/commond/sort/QuickSort.java
@@ -7,8 +7,12 @@ import java.util.Set;
 /**
  * 排序算法之快速排序
  * http://3ms.huawei.com/km/blogs/details/5973811
+ * <p>
+ * <p>
+ * 平均时间复杂度	最好情况	    最坏情况	空间复杂度
+ * O(nlog₂n)	O(nlog₂n)	O(n²)	O(1)（原地分区递归版）
  */
-public class QuickSort {
+public class QuickSort implements ISort {
 
     public static void main(String[] args) {
         int[] ints = {6,5,4,3,2,1};
@@ -43,5 +47,10 @@ public class QuickSort {
         ints[i] = temp;
         sort(ints, left, i - 1);
         sort(ints, i + 1, right);
+    }
+
+    @Override
+    public void sort(int[] arr) {
+        sort(arr, 0, arr.length - 1);
     }
 }
