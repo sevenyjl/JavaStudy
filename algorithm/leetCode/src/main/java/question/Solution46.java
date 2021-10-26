3a985ab2380628b2e0c94bea567e6381dde0691d
sevenyjl <a6d5b748a0974138bbfe4d403b2acd12@example.com>
2021-10-26 17:12:22 +0800

    Demo46全排列

---
+++ algorithm/leetCode/src/main/java/question/Solution46.java
@@ -0,0 +1,79 @@
+package question.Demo46全排列;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+
+/**
+ * 给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：nums = [1,2,3]
+ * 输出：[[1,2,3],[1,3,2],[3,1,2],[2,1,3],[2,3,1],[3,2,1]]
+ * <p>
+ * <p>
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：nums = [0,1]
+ * 输出：[[0,1],[1,0]]
+ * <p>
+ * <p>
+ * 示例 3：
+ * <p>
+ * <p>
+ * 输入：nums = [1]
+ * 输出：[[1]]
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * <p>
+ * 1 <= nums.length <= 6
+ * -10 <= nums[i] <= 10
+ * nums 中的所有整数 互不相同
+ * <p>
+ * Related Topics 数组 回溯
+ * 👍 1601 👎 0
+ */
+public class Solution46 {
+    public static void main(String[] args) {
+        System.out.println(permute(new int[] {1, 2, 3}));
+    }
+
+    public static List<List<Integer>> permute(int[] nums) {
+        ArrayList<Integer> integers = new ArrayList<>();
+        for (int num : nums) {
+            integers.add(num);
+        }
+        ArrayList<List<Integer>> result = new ArrayList<>();
+        backtrack(nums.length, integers, result, 0);
+        return result;
+
+    }
+
+    public static void backtrack(int n, List<Integer> output, List<List<Integer>> res, int first) {
+        // 所有数都填完了
+        if (first == n) {
+            res.add(new ArrayList<Integer>(output));
+        }
+        for (int i = first; i < n; i++) {
+            // 动态维护数组
+            Collections.swap(output, first, i);
+            // 继续递归填下一个数
+            backtrack(n, output, res, first + 1);
+            // 撤销操作
+            Collections.swap(output, first, i);
+        }
+    }
+}
